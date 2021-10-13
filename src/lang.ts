da3ea2a028d1f142ecdf87a01881e8b3ea85f835
Nguyễn Hoàng Minh <nghminh163@outlook.com>
2021-10-13 23:51:08 +0700

    Added read wallet feature

---
+++ src/lang.ts
@@ -0,0 +1,6 @@
+export default {
+  ADDRESS_ETH_INVALID: `"wallet_address" is invalid with Ethereum address`,
+  BAD_REQUEST: 'Bad Request',
+  WALLET_NOT_UNIQUE: `"wallet_address" is not unique`,
+  SUCCESS: 'Successful',
+};
